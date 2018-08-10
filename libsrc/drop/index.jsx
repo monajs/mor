0a4a7818fb97617009d1618e9aef92802f785107
yangxi <599321378@qq.com>
2018-08-10 12:29:43 +0800

    feat: 优化drop 功能

--- libsrc/drop/index.jsx
+++ libsrc/drop/index.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+import { findDOMNode } from 'react-dom'
 import classNames from 'classnames'
 import DropTitle from './title'
 import DropContent from './content'
 import DropCtrl from './ctrl'
+import './index.less'
 
 export default class Drop extends Component {
 	constructor (props) {
@@ -29,6 +31,23 @@ export default class Drop extends Component {
 		this.ctrl.off('monaDropCtrl')
 	}
 	
+	optionChildren (props) {
+		const { children, isOpen = false } = props
+		return React.Children.map(children, v => {
+			if (!v) {
+				return
+			}
+			if (v.type === DropTitle || v.type === DropContent) {
+				return React.cloneElement(v, { isOpen: isOpen || false, ctrl: this.ctrl })
+			} else {
+				if (v.props && v.props.children) {
+					return React.cloneElement(v, { children: this.optionChildren(v.props) })
+				}
+				return v
+			}
+		})
+	}
+	
 	render () {
 		const {
 			className,
@@ -38,17 +57,7 @@ export default class Drop extends Component {
 			...props
 		} = this.props
 		
-		let child = children
-		child = React.Children.map(children, (v) => {
-			if (!v) {
-				return
-			}
-			if (v.type === DropTitle || v.type === DropContent) {
-				return React.cloneElement(v, { isOpen: isOpen, ctrl: this.ctrl })
-			} else {
-				return v
-			}
-		})
+		let child = this.optionChildren(this.props)
 		
 		return (
 			<div className={classNames('mona-drop', className)} {...props}>
